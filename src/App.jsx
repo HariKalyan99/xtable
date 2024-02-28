c8a2c90cf47854209c9e65e499f438beceb73f0d
Harikalyan99 <harikalyan99tronics@gmail.com>
2024-02-28 19:32:10 +0530

    xtable

--- src/App.jsx
+++ src/App.jsx
@@ -17,13 +17,37 @@ function App() {
 
 ]
 
-const [sortArticleTable, setSortArticleTable] = useState(articleTable)
+const [sortArticleTable, setSortArticleTable] = useState([
+
+    { date: "2022-09-01", views: 100, article: "Article 1" },
+
+    { date: "2023-09-01", views: 100, article: "Article 1" },
+
+    { date: "2023-09-02", views: 150, article: "Article 2" },
+
+    { date: "2023-09-02", views: 120, article: "Article 3" },
+
+    { date: "2020-09-03", views: 200, article: "Article 4" }
+
+])
+
+
+const sortByDate = () => {
+  let sortedArticleByDate = articleTable.sort((a,b) => b.date.localeCompare(a.date));
+  setSortArticleTable(sortedArticleByDate);
+}
+
+const sortByViews = () => {
+  let sortedArticleByViews = articleTable.sort((a,b) => b.views - a.views);
+  setSortArticleTable(sortedArticleByViews);
+}
 
   return (
     <div>
       <h1>Date and Views Table</h1>
-      <button>Sort by Date</button>
-      <button>Sort by Views</button>
+      <br />
+      <button onClick={sortByDate}>Sort by Date</button>
+      <button onClick={sortByViews}>Sort by Views</button>
       <table>
         <thead>
           <tr>
@@ -33,8 +57,8 @@ const [sortArticleTable, setSortArticleTable] = useState(articleTable)
           </tr>
         </thead>
         <tbody>
-            {sortArticleTable.map((article, ind) => {
-              return <tr key={article.date}>
+            {sortArticleTable.length > 0 &&  sortArticleTable.map((article, ind) => {
+              return <tr key={ind}>
                 <td>{article.date}</td>
                 <td>{article.views}</td>
                 <td>{article.article}</td>
